945d4f7ec819a1d33e4df79e13e737f2e30ffbc9
patschramm <>
2021-08-02 17:21:46 -0700

    vowel cipher

--- ruby.rb
+++ ruby.rb
@@ -1,8 +1,20 @@
 def caesar_cipher(str, num)
   alph = "abcdefghijklmnopqrstuvwxyz"
   res = ''
-  str.each_char do |char|
-    res += alphabet[(alphabet.index(char) + num) % 26]
-  end
-  return res
+  str.each_char { |char| res += alphabet[(alphabet.index(char) + num) % 26]}
+  res
 end
+
+def vowel_cipher(string)
+  vowels = 'aeiou'
+  string.each_char.with_index do |char, i|
+    if vowels.include?(char)
+      string[i] = vowels[(vowels.index(char) + 1) % 5]
+    end
+  end
+
+  string
+end
+
+puts vowel_cipher("bootcamp")
+puts vowel_cipher("paper cup")
