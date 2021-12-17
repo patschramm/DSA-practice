b7d6dc548eaaaf62c08a7adfda285a3ac0ae9b4f
patschramm <patrickcharlesschramm@gmail.com>
2021-12-16 21:20:07 -0800

    DSA JS

--- js.js
+++ js.js
@@ -1,24 +1,3 @@
-var areOccurrencesEqual = function (s) {
-  let hash = {};
-
-  for (let i = 0; i < s.length; i++) {
-    if (hash[s[i]] !== undefined) {
-      hash[s[i]]++;
-    } else {
-      hash[s[i]] = 1;
-    }
-  }
-
-  let n = hash[s[0]];
-
-  for (key in hash) {
-    if (hash[key] !== n) {
-      return false;
-    }
-  }
-
-  return true;
-};
 
 
 var countBalls = function (lowLimit, highLimit) {
